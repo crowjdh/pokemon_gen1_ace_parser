beb8b919ca2cec405deb615b9c93e5a92fe47a2c
ChrisJeong <crowjdh@gmail.com>
2022-06-21 10:37:14 +0900

    Improve output formatting

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
+import sys
 from parser import Parser
 from command import TERMINAL_COMMAND
 
-def main():
-  with open('samples/raw_command.txt', 'r') as raw_command_file:
+def main(cmd_file_path):
+  with open(cmd_file_path, 'r') as raw_command_file:
     raw_command_lines = raw_command_file.readlines()
     parser = Parser(raw_command_lines)
     parser.parse()
 
     for command in parser.commands:
-      print(command.description)
-      if command == TERMINAL_COMMAND:
-        print()
+      print_command(command)
+
+def print_command(command):
+  description = command.description
+  if command == TERMINAL_COMMAND:
+    description = f'({description})\n'
+
+  print(description)
 
 
 if __name__ == '__main__':
-  main()
+  if len(sys.argv) < 2:
+    print("Usage: python main.py PATH_TO_RAW_CMD_FILE")
+    exit(-1)
+
+  cmd_file_path = sys.argv[1]
+  main(cmd_file_path)
